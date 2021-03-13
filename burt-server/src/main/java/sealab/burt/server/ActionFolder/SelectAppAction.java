b7c0cbb24aa1331427cb91cdb9311799b6058dc4
Oscar Chaparro <ojcch1@gmail.com>
2021-03-12 19:45:17 -0500

    Creating intent tokens

--- burt-server/src/main/java/sealab/burt/server/ActionFolder/SelectAppAction.java
+++
@@ -1,7 +0,0 @@
-class SelectAppAction implements ChatbotAction{
-    public static String execute(messageObj messageObject){
-        String reponseMsg = messageObject.message
-        changeState("WELCOME_GIVEN");
-        return String;
-    }
-}
