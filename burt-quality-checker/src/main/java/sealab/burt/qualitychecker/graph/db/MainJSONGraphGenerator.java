07b380939cbf230989149cd41e91c78503c90cc8
Oscar Chaparro <ojcch1@gmail.com>
2021-07-01 15:17:58 -0400

    Removing Android Core dependency

--- burt-quality-checker/src/main/java/sealab/burt/qualitychecker/graph/db/MainJSONGraphGenerator.java
+++ burt-quality-checker/src/main/java/sealab/burt/qualitychecker/graph/db/MainJSONGraphGenerator.java
@@ -49,7 +49,8 @@ class MainJSONGraphGenerator {
 
     public static void main(String[] args) throws Exception {
 
-        AppGraphInfo graphInfo = JSONGraphReader.getGraph("mileage", "3.1.1");
+//        AppGraphInfo graphInfo = JSONGraphReader.getGraph("mileage", "3.1.1");
+        AppGraphInfo graphInfo = JSONGraphReader.getGraph("GnuCash", "2.1.3");
         AppGraph<GraphState, GraphTransition> graph = graphInfo.getGraph();
         Appl app = graphInfo.getApp();
 
