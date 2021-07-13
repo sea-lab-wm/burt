d1da5333461dfadb3b32ed6b7d264d38f48c3442
Yang Song <ysong10@email.wm.edu>
2021-07-13 14:52:41 -0400

    improve gui

--- burt-gui/src/components/ReportGenerator.jsx
+++ burt-gui/src/components/ReportGenerator.jsx
@@ -1,13 +1,14 @@
 import React , { useState, useEffect }  from "react";
 import config from "../config";
-
+import "./ReportGenerator.css"
 const ReportGenerator = (props) => {
     const generatedReport = props.generatedReport;
     console.log("Generated report: " +generatedReport);
     const link = config.serverEndpoint +"/" + generatedReport
 
     return (
-<div>   <a target="_blank" href={link}>Bug report</a></div>
+       <a target="_blank" href={link}>
+    <button className="bn633-hover bn28">bug report</button> </a>
     )
 
 }
