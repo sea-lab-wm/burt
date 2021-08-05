1d438a347e4c9f1fb476ee55c9abb53edcfd2dcf
Oscar Chaparro <ojcch1@gmail.com>
2021-08-05 18:13:00 +0000

    adding file logging to screen commands

--- run_chatbot.sh
+++ run_chatbot.sh
@@ -3,11 +3,11 @@ export CUR_DIR=`pwd`
 cd $CUR_DIR
 cd burt-server
 screen_name="burt-server"
-screen -dmS $screen_name
+screen -dmSL $screen_name
 screen -S $screen_name -X screen ./update_deps_and_run_server.sh
 
 cd $CUR_DIR
 cd burt-gui
 screen_name="burt-gui"
-screen -dmS $screen_name
+screen -dmSL $screen_name
 screen -S $screen_name -X screen ./run_app.sh
