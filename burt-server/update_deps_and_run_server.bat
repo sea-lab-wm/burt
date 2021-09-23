5a22874c8ebf0829febda238ee19ad3f8c6045a6
Yang Song <ysong10@email.wm.edu>
2021-09-23 11:33:10 -0400

    update

--- burt-server/update_deps_and_run_server.bat
+++ burt-server/update_deps_and_run_server.bat
@@ -23,7 +23,7 @@ cd "%BUG_REPORT_COMPLETION_REPO_PATH%\code\bug_report_coding" && call gradlew cl
 cd "%BUG_REPORT_COMPLETION_REPO_PATH%\code\bug_report_patterns" && call gradlew clean testClasses install && @echo on
 cd "%BUG_REPORT_COMPLETION_REPO_PATH%\code\bug_report_classifier" && call gradlew clean testClasses install && @echo on
 cd "%BUG_REPORT_COMPLETION_REPO_PATH%\code\bug_report_parser\bugparser" && call gradlew clean testClasses install && @echo on
-cd "%ANDROID_CORE_REPO_PATH%" && call mvn clean install -DskipTests && @echo on
+rem cd "%ANDROID_CORE_REPO_PATH%" && call mvn clean install -DskipTests && @echo on
 
 cd "%CUR_DIR%"
 
