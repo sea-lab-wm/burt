d13f3cb24438d635393fe16bb24819fccb668249
Yang Song <ysong10@email.wm.edu>
2022-05-10 16:22:56 -0400

    update statistics evaluation result

---
+++ evaluation/code/statistics_test/mann-whitney-test.py
@@ -0,0 +1,124 @@
+from scipy.stats import mannwhitneyu
+import xlrd
+import pandas as pd
+
+def get_burt_data():
+    sheet_incorrect = pd.read_excel('burt_data.xlsx', usecols=[24], names=None)
+    incorrect_list = sheet_incorrect.values.tolist()
+    incorrect_steps = []
+    for li in incorrect_list:
+        incorrect_steps.append(li[0])
+
+    sheet_missing = pd.read_excel('burt_data.xlsx', usecols=[26], names=None)
+    missing_list = sheet_missing.values.tolist()
+    missing_steps = []
+    for li in missing_list:
+        missing_steps.append(li[0])
+
+    return incorrect_steps, missing_steps
+
+
+def get_itrac_data():
+    sheet_incorrect = pd.read_excel('itrac_data.xlsx', usecols=[11], names=None)
+    incorrect_list = sheet_incorrect.values.tolist()
+    incorrect_steps = []
+    for li in incorrect_list:
+        incorrect_steps.append(li[0])
+
+    sheet_missing = pd.read_excel('itrac_data.xlsx', usecols=[12], names=None)
+    missing_list = sheet_missing.values.tolist()
+    missing_steps = []
+    for li in missing_list:
+        missing_steps.append(li[0])
+
+    return incorrect_steps, missing_steps
+
+
+def get_burt_ob_eb_data():
+    sheet_incorrect = pd.read_excel('burt_data.xlsx', usecols=[28], names=None)
+    ob_list = sheet_incorrect.values.tolist()
+    ob = []
+    for li in ob_list:
+        ob.append(li[0])
+
+    sheet_incorrect = pd.read_excel('burt_data.xlsx', usecols=[29], names=None)
+    eb_list = sheet_incorrect.values.tolist()
+    eb = []
+    for li in eb_list:
+        eb.append(li[0])
+
+    return ob, eb
+
+def get_itrac_ob_eb_data():
+    sheet_incorrect = pd.read_excel('itrac_data.xlsx', usecols=[7], names=None)
+    ob_list = sheet_incorrect.values.tolist()
+    ob = []
+    for li in ob_list:
+        ob.append(li[0])
+
+    sheet_incorrect = pd.read_excel('itrac_data.xlsx', usecols=[8], names=None)
+    eb_list = sheet_incorrect.values.tolist()
+    eb = []
+    for li in eb_list:
+        eb.append(li[0])
+
+    return ob, eb
+
+
+
+
+def compute_mann_whiteney():
+    burt_incorrect_steps, burt_missing_steps = get_burt_data()
+    itrac_incorrect_steps, itrac_missing_steps = get_itrac_data()
+    burt_ob, burt_eb = get_burt_ob_eb_data()
+    itrac_ob, itrac_eb = get_itrac_ob_eb_data()
+
+    print(burt_missing_steps, sum(burt_missing_steps))
+
+    print(itrac_missing_steps, sum(itrac_missing_steps))
+
+    print(burt_incorrect_steps, sum(burt_incorrect_steps))
+    print(itrac_incorrect_steps, sum(itrac_incorrect_steps))
+
+    print(burt_ob, sum(burt_ob))
+    print(burt_eb, sum(burt_eb))
+    print(itrac_ob, sum(itrac_ob))
+    print(itrac_eb, sum(itrac_eb))
+
+
+
+    res_missing = mannwhitneyu(burt_missing_steps, itrac_missing_steps, alternative="less", method="auto")
+
+    print(res_missing)
+
+    res_incorrect = mannwhitneyu(burt_incorrect_steps, itrac_incorrect_steps, alternative="less", method="auto")
+
+    print(res_incorrect)
+
+    res_ob = mannwhitneyu(burt_ob, itrac_ob, alternative="less", method="auto")
+
+    print(res_ob)
+
+    res_eb = mannwhitneyu(burt_eb, itrac_eb, alternative="less", method="auto")
+
+    print(res_eb)
+
+    res_ob_itrac = [0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 2, 1]
+    res_ob_burt = [1,1,2,3,0,1,0,3,1,2,2,0]
+    res_eb_itrac = [0,1,1,1,1,0,0,1,0,2,0,1]
+    res_eb_burt =  [3,0,1,1,0,0,0,0,0,1,0,0]
+
+    res_ob_1 = mannwhitneyu(res_ob_burt, res_ob_itrac, alternative="less", method="auto")
+
+    print(res_ob_1)
+
+    res_eb_1 = mannwhitneyu(res_eb_burt, res_eb_itrac, alternative="less", method="auto")
+
+    print(res_eb_1)
+
+
+compute_mann_whiteney()
+
+
+
+
