6269125729ef9e50201a7541e52df3ebd50a4261
Yang Song <ysong10@email.wm.edu>
2022-05-11 22:18:38 -0400

    update statistics evaluation result

--- evaluation/code/statistics_test/mann-whitney-test.py
+++ evaluation/code/statistics_test/mann-whitney-test.py
@@ -119,7 +119,7 @@ def draw_box_plots(data1, data2, data3, data4):
     # showmeans=True, meanline=True,
 
     bplot2 = axes[1].boxplot(all_data[2:4],
-                             notch=True,
+                             # notch=True,
                              vert=True,
                              patch_artist=True, showfliers=False, showmeans=True, meanline=False)
     colors = ['pink', 'lightblue']
@@ -129,8 +129,10 @@ def draw_box_plots(data1, data2, data3, data4):
     plt.setp(axes, xticks=[1, 2],
              xticklabels=['burt', 'itrac'])
 
-    axes[0].set_title("incorrect steps", fontsize=15)
-    axes[1].set_title("missing steps", fontsize=15)
+    # axes[0].set_title("incorrect steps", fontsize=15)
+    # axes[1].set_title("missing steps", fontsize=15)
+    axes[0].set_title("OB", fontsize=15)
+    axes[1].set_title("EB", fontsize=15)
     plt.show()
 
 
@@ -200,8 +202,142 @@ def compute_cliff_delta_all():
     print("eb per bug", res_eb_per_bug, d_eb_per_bug)
 
 
+def get_bug_experience_data(file_name):
+    sheet_incorrect_novice = pd.read_excel(file_name, usecols=[0], names=None)
+    incorrect_steps_list_novice = sheet_incorrect_novice.values.tolist()
+    incorrect_steps_novice = []
+    for li in incorrect_steps_list_novice:
+        incorrect_steps_novice.append(li[0])
+
+    sheet_missing_novice = pd.read_excel(file_name, usecols=[1], names=None)
+    missing_list_novice = sheet_missing_novice.values.tolist()
+    missing_steps_novice = []
+    for li in missing_list_novice:
+        missing_steps_novice.append(li[0])
+
+
+    sheet_incorrect_intermediate = pd.read_excel(file_name, usecols=[2], names=None)
+    incorrect_steps_list_intermediate = sheet_incorrect_intermediate.values.tolist()
+    incorrect_steps_intermediate = []
+    for li in incorrect_steps_list_intermediate:
+        incorrect_steps_intermediate.append(li[0])
+
+    sheet_missing_intermediate = pd.read_excel(file_name, usecols=[3], names=None)
+    missing_list_intermediate = sheet_missing_intermediate.values.tolist()
+    missing_steps_intermediate = []
+    for li in missing_list_intermediate:
+        missing_steps_intermediate.append(li[0])
+
+
+    sheet_incorrect_experienced = pd.read_excel(file_name, usecols=[4], names=None)
+    incorrect_steps_list_experienced = sheet_incorrect_experienced.values.tolist()
+    incorrect_steps_experienced = []
+    for li in incorrect_steps_list_experienced:
+        incorrect_steps_experienced.append(li[0])
+
+    sheet_missing_experienced = pd.read_excel(file_name, usecols=[5], names=None)
+    missing_list_experienced = sheet_missing_experienced.values.tolist()
+    missing_steps_experienced = []
+    for li in missing_list_experienced:
+        missing_steps_experienced.append(li[0])
+
+
+    return incorrect_steps_novice, missing_steps_novice, incorrect_steps_intermediate, \
+           missing_steps_intermediate, incorrect_steps_experienced, missing_steps_experienced
+
+
+
+
+def test_bug_experience():
+    incorrect_steps_novice_burt, missing_steps_novice_burt, incorrect_steps_intermediate_burt, missing_steps_intermediate_burt, \
+    incorrect_steps_experienced_burt, missing_steps_experienced_burt = get_bug_experience_data('bug_reporting_experience_burt.xlsx')
+
+    incorrect_steps_novice_itrac, missing_steps_novice_itrac, incorrect_steps_intermediate_itrac, missing_steps_intermediate_itrac, \
+    incorrect_steps_experienced_itrac, missing_steps_experienced_itrac = get_bug_experience_data('bug_reporting_experience_itrac.xlsx')
+
+    incorrect_steps_novice_burt = [x for x in incorrect_steps_novice_burt if str(x) != 'nan']
+    missing_steps_novice_burt = [x for x in missing_steps_novice_burt if str(x) != 'nan']
+    incorrect_steps_intermediate_burt = [x for x in incorrect_steps_intermediate_burt if str(x) != 'nan']
+    missing_steps_intermediate_burt = [x for x in missing_steps_intermediate_burt if str(x) != 'nan']
+    incorrect_steps_experienced_burt = [x for x in incorrect_steps_experienced_burt if str(x) != 'nan']
+    missing_steps_experienced_burt = [x for x in missing_steps_experienced_burt if str(x) != 'nan']
+
+    incorrect_steps_novice_itrac = [x for x in incorrect_steps_novice_itrac if str(x) != 'nan']
+    missing_steps_novice_itrac = [x for x in missing_steps_novice_itrac if str(x) != 'nan']
+    incorrect_steps_intermediate_itrac = [x for x in incorrect_steps_intermediate_itrac if str(x) != 'nan']
+    missing_steps_intermediate_itrac = [x for x in missing_steps_intermediate_itrac if str(x) != 'nan']
+    incorrect_steps_experienced_itrac = [x for x in incorrect_steps_experienced_itrac if str(x) != 'nan']
+    missing_steps_experienced_itrac = [x for x in missing_steps_experienced_itrac if str(x) != 'nan']
+
+    print(incorrect_steps_novice_burt)
+    print(missing_steps_novice_burt)
+    print(incorrect_steps_intermediate_burt)
+    print(missing_steps_intermediate_burt)
+    print(incorrect_steps_experienced_burt)
+    print(missing_steps_experienced_burt)
+
+
+    print(incorrect_steps_novice_itrac)
+    print(missing_steps_novice_itrac)
+    print(incorrect_steps_intermediate_itrac)
+    print(missing_steps_intermediate_itrac)
+    print(incorrect_steps_experienced_itrac)
+    print(missing_steps_experienced_itrac)
+
+
+
+    res_missing_novice = mannwhitneyu(missing_steps_novice_burt, missing_steps_novice_itrac, alternative="less", method="auto")
+
+    print("res_missing_novice", res_missing_novice)
+
+    res_missing_steps_novice_cliff, d_missing_steps_novice_cliff = compute_cliffs_delta(missing_steps_novice_burt, missing_steps_novice_itrac)
+    print("missing_steps_novice_cliff", res_missing_steps_novice_cliff, d_missing_steps_novice_cliff)
+
+
+    res_incorrect_novice = mannwhitneyu(incorrect_steps_novice_burt, incorrect_steps_novice_itrac, alternative="less", method="auto")
+
+    print("res_incorrect_novice", res_incorrect_novice)
+
+    res_incorrect_novice_cliff, d_incorrect_novice_cliff = compute_cliffs_delta(incorrect_steps_novice_burt, incorrect_steps_novice_itrac)
+    print("incorrect_novice_cliff", res_incorrect_novice_cliff, d_incorrect_novice_cliff)
+
+
+    res_missing_intermediate = mannwhitneyu(missing_steps_intermediate_burt, missing_steps_intermediate_itrac, alternative="less", method="auto")
+
+    print("res_missing_intermediate", res_missing_intermediate)
+
+    res_missing_intermediate_cliff, d_missing_intermediate_cliff = compute_cliffs_delta(missing_steps_intermediate_burt, missing_steps_intermediate_itrac)
+    print("missing_intermediate_cliff", res_missing_intermediate_cliff, d_missing_intermediate_cliff)
+
+
+
+    res_incorrect_intermediate = mannwhitneyu(incorrect_steps_intermediate_burt, incorrect_steps_intermediate_itrac, alternative="less", method="auto")
+
+    print("res_incorrect_intermediate", res_incorrect_intermediate)
+
+    res_incorrect_intermediate_cliff, d_incorrect_intermediate_cliff = compute_cliffs_delta(incorrect_steps_intermediate_burt, incorrect_steps_intermediate_itrac)
+    print("incorrect_intermediate_cliff", res_incorrect_intermediate_cliff, d_incorrect_intermediate_cliff)
+
+
+    res_missing_experienced = mannwhitneyu(missing_steps_experienced_burt, missing_steps_experienced_itrac, alternative="less", method="auto")
+
+    print("res_missing_experienced", res_missing_experienced)
+
+    res_missing_experienced_cliff, d_missing_experienced_cliff = compute_cliffs_delta(missing_steps_experienced_burt, missing_steps_experienced_itrac)
+    print("incorrect_novice_cliff", res_missing_experienced_cliff, d_missing_experienced_cliff)
+
+    res_incorrect_experienced = mannwhitneyu(incorrect_steps_experienced_burt, incorrect_steps_experienced_itrac, alternative="less", method="auto")
+
+    print("res_incorrect_experienced", res_incorrect_experienced)
+
+    res_incorrect_experienced_cliff, d_incorrect_experienced_cliff = compute_cliffs_delta(incorrect_steps_experienced_burt, incorrect_steps_experienced_itrac)
+    print("incorrect_incorrect_experienced_cliff", res_incorrect_experienced_cliff, d_incorrect_experienced_cliff)
+
+
+
+
 if __name__ == '__main__':
-    compute_mann_whiteney()
+    # compute_mann_whiteney()
     '''per bug for missing and incorrect steps'''
     burt_incorrect_steps_per_bug, burt_missing_steps_per_bug = get_burt_data_per_bug()
     itrac_incorrect_steps_per_bug, itrac_missing_steps_per_bug = get_itrac_data_per_bug()
@@ -215,7 +351,14 @@ if __name__ == '__main__':
     # draw_box_plots(burt_incorrect_steps, itrac_incorrect_steps,
     #                burt_missing_steps, itrac_missing_steps)
 
+    '''per bug report for OB and EB'''
+
+    draw_box_plots(res_ob_burt, res_ob_itrac,
+                   res_eb_burt, res_eb_itrac)
+
     '''compute cliffs delta'''
     compute_cliff_delta_all()
+
+    test_bug_experience()
 
 
