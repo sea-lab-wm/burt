2dbd33f4f908f92cc0d9f5ecbd8cf21ec6504315
zhouyinn <yinnzho@gmail.com>
2023-05-12 21:22:04 +0000

    deploy it in rocco server

--- run_chatbot.sh
+++ run_chatbot.sh
@@ -7,7 +7,7 @@ screen -dmSL $screen_name
 screen -S $screen_name -X screen ./update_deps_and_run_server.sh
 
 cd $CUR_DIR
-cd burt-gui-new
+cd burt-gui
 screen_name="burt-gui"
 screen -dmSL $screen_name
 screen -S $screen_name -X screen ./run_app.sh
